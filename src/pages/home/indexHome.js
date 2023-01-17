01883340ba2ae1185d9bebfacc7f37442a5c7110
Aline Pereira <dominguespereiraaline@gmail.com>
2023-01-17 20:41:43 -0300

    Ex aula 04 - cria rotas para login e home

--- src/pages/home/indexHome.js
+++ src/pages/home/indexHome.js
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/indexButton";
+import Imagem from "../../assets/png/urban-line-305.png";
+import Gif from "../../assets/gif/urban-line-305.gif";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Home(props) {
@@ -11,15 +13,22 @@ function Home(props) {
 
   return (
     <>
-      <div class="bg-dark bg-opacity-60 p-2 px-3 d-flex justify-content-between align-items-center sticky-top">
-        <h2 class="text-secondary fw-light">
-          Home<span class="fw-semibold fst-italic text-light">Page</span>
+      <div className="bg-dark bg-opacity-60 p-2 px-3 d-flex justify-content-between align-items-center sticky-top">
+        <h2 className="text-secondary fw-light">
+          Home<span className="fw-semibold fst-italic text-light">Page</span>
           <span class="fw-light display-7 text-light">
             <i class="bi bi-book"></i>
           </span>
         </h2>
         <Button button="Sair" redirecionar={sair} />
       </div>
+      <img className="img-fluid rounded mx-auto d-block mt-20" src={Gif} />
+      <h4 className="text-dark fw-light text-center mt-5">
+        OOOPS<span className="fw-semibold fst-italic text-secondary"> Algo deu errado :/</span>
+        <span class="fw-light display-7 text-light">
+          <i class="bi bi-book"></i>
+        </span>
+      </h4>
     </>
   );
 }
